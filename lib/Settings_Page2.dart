a24b7e9685e09212dfc8619d7414b333318fa0fb
Integersplus <integersplus@gmail.com>
2025-04-20 09:57:00 -0400

    Update settings_screen2.dart

--- lib/Settings_Page2.dart
+++ lib/Settings_Page2.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 import 'package:provider/provider.dart';
-
+ 
 class SettingsPage extends StatefulWidget {
   final Function(Color) onAppBarColorChanged;
 
